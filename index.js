cba0f074d3d794b33514d02fa99be6a7eeacb98c
Hoang Phan <hpha0005@student.monash.edu>
2021-07-12 13:39:17 +1000

    Getting the bot to join and leave the same voice channel as the victim

--- index.js
+++ index.js
@@ -5,13 +5,41 @@ import command from './command';
 dotenv.config();
 
 const client = new Client()
+var victim
+
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in!`)
 
-    // Using the command handler to get the bot respond to "!ping" with "pong"
-    command(client, 'ping', (message) => {
-        message.channel.send('pong!')
+    command(client, 'annoy', (message) => {
+        victim = message.mentions.users.first()
     })
+})
+
+client.on('voiceStateUpdate', async (oldMember, newMember) => {
+    if (victim) {
+        // Check if the member is the same as the victim
+        if (victim.id === oldMember.member.id) {
+            let newUserChannel = newMember.channel
+            let oldUserChannel = oldMember.channel
+
+            if (!oldUserChannel && newUserChannel) {
+                newUserChannel.join();
+            } else if (!newUserChannel) {
+                console.log("REACHED")
+                const guildId = oldMember.guild.id;
+                const clientVoiceConnection = await client.guilds.fetch(guildId).then( (guild) => guild.voice )
+                if (clientVoiceConnection) {
+                    console.log(clientVoiceConnection)
+                    clientVoiceConnection.channel.leave()
+                }
+            }
+
+        } else {
+            return
+        }
+    } else {
+        return
+    }
 })
 
 client.login(process.env.DISCORD_TOKEN);
