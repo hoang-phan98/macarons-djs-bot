9780c87b80668ac31293d9d621e5d499deccbd1f
Hoang Phan <hpha0005@student.monash.edu>
2021-07-14 14:37:30 +1000

    Adding comments and cleaning up syntax

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
-import { Client, MessageAttachment } from 'discord.js';
 import path from 'path';
-import { Client } from 'discord.js';
+import axios from 'axios';
+import Discord, { Client } from 'discord.js';
 import command from './command';
 import { audio } from "./config.json";
 
@@ -11,16 +11,13 @@ const client = new Client()
 var victim
 var dispatcher
 
-// Meme API, need to install axios
-const axios = require('axios');
-const Discord = require('discord.js');
-
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in!`)
 
     command(client, 'annoy', (message) => {
         victim = message.mentions.users.first()
-        console.log(message.guild.members.cache.get(victim.id))
+
+        // join the same voice channel as the victim
         const member = message.guild.members.cache.get(victim.id)
         const channel = member.voice.channel
         if (channel) {
@@ -32,15 +29,15 @@ client.on('ready', () => {
     command(client, 'meme', (message) => {
         // Get the meme
         axios.get('https://www.reddit.com/r/memes/.json')
-            .then((res)=>{
+            .then((res) => {
                 // Random number
                 let number = Math.floor(Math.random() * res.data.data.children.length)
 
                 //Create the embed message
                 const embed = new Discord.MessageEmbed()
-                .setTitle('Here is the meme of the day!')
-                .setImage(res.data.data.children[number].data.url)
-                
+                    .setTitle('Here is the meme of the day!')
+                    .setImage(res.data.data.children[number].data.url)
+
                 //Send message to the chat
                 message.channel.send(embed)
             })
@@ -51,14 +48,14 @@ client.on('voiceStateUpdate', async (oldMember, newMember) => {
     if (victim) {
         // Check if the member is the same as the victim
         if (victim.id === oldMember.member.id) {
-            
+
             let newUserChannel = newMember.channel
 
             // Victim joins a new channel
             if (newUserChannel) {
                 newUserChannel.join();
 
-            // Victim disconnects from the channel
+                // Victim disconnects from the channel
             } else {
                 const guildId = oldMember.guild.id;
                 const clientVoiceConnection = await client.guilds.fetch(guildId).then((guild) => guild.voice)
@@ -68,9 +65,6 @@ client.on('voiceStateUpdate', async (oldMember, newMember) => {
                     clientVoiceConnection.channel.leave()
                 }
             }
-
-            // TODO: Get the bot to follow victims across different channels
-
         }
     }
 })
@@ -85,12 +79,16 @@ client.on('guildMemberSpeaking', (member, speaking) => {
                 console.log("Speaking")
                 if (clientVoiceConnection) {
                     if (dispatcher) {
+                        // Resume the audio if the dispatcher exists
                         dispatcher.resume()
+
+                        // Set the dispatcher to null when audio file finishes
                         dispatcher.on('finish', () => {
                             console.log("Finished")
                             dispatcher = undefined
                         })
                     } else {
+                        // Create a new dispatcher and play/replay the audio file
                         dispatcher = clientVoiceConnection.play(path.join(__dirname, audio))
                     }
                 }
